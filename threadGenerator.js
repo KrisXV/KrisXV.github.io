13ed052c882584a68c5728201629547d3b010726
IFM-HODOR <IFMstudios@gmail.com>
2016-10-02 15:41:58 -0400

    Updating Thread Generator

--- threadGenerator.js
+++ threadGenerator.js
@@ -1,22 +1,28 @@
 var app = angular.module('threadApp', []);
 app.controller('threadCtrl', function($scope) {
+	
 	$scope.username = "";
-	$scope.tier = "";
-	$scope.week = 1;
-	$scope.tourNumber = 1;
+	$scope.tier = "ORAS OU";
+	$scope.week = "1";
+	$scope.tourNumber = 0;
 	$scope.title = "";
 	$scope.initial = true;
 	
 	$scope.generateOP = function() {
-		$scope.title = "Season 22 (Week " + $scope.week + " [" + $scope.tier + " #" + $scope.tourNumber + "])";
 		$scope.initial = false;
+		var date = new Date();
+		var d = date.getDay();
+		if(d === 5){ $scope.tourNumber = 1; }
+		if(d === 6){ $scope.tourNumber = 2; }
+		if(d === 0){ $scope.tourNumber = 3; }
+		$scope.title = "Season 22 (Week " + $scope.week + " [" + $scope.tier + " #" + $scope.tourNumber + "])";
 	}
 	
 	$scope.reset = function() {
 		$scope.username = "";
-		$scope.tier = "";
-		$scope.week = 1;
-		$scope.tourNumber = 1;
+		$scope.tier = "ORAS OU";
+		$scope.week = "1";
+		$scope.tourNumber = 0;
 		$scope.title = "";
 		$scope.initial = true;
 	}
