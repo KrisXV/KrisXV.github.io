277dc717c41be1c144b120be016f9263fb2115ab
IFM-HODOR <IFMstudios@gmail.com>
2016-12-06 18:06:53 -0500

    Refactor to use actual forum name

--- spl-info.js
+++ spl-info.js
@@ -1,72 +1,84 @@
 var app = angular.module('splInfoApp', []);
 app.controller('splInfoCtrl', function($scope) {
-	$scope.htmlInput = "";
+	$scope.htmlInput = [];
 	$scope.players = [];
 	$scope.csvExport;
 	
 	$scope.generateCSV = function(){
-		partial = $scope.htmlInput.split(/(Player name:|Player Name:|player name:)/g);
-		partial.forEach(function(p) {
-			if(p.includes('Tiers Played') || p.includes('Tiers played') || p.includes('tiers played')){
-		    	partial2 = p.split(/(Tiers Played:|Tiers played:|tiers played:)/g);
-		    	playerName = partial2[0].trim();
-		    	partial3 = partial2[1].split(/(Timezone:|timezone:)/g);
-		    	tiers = partial3[0].trim();
-		    	player = { 'Name': playerName }
-		    	if(tiers.toLowerCase().includes('sm ou') || tiers.toLowerCase().includes('sumo ou')){
+		var player;
+		$scope.htmlInput.forEach(function(line) {
+			if(line.includes('Player name:') || line.includes('Player Name:') || line.includes('player name')) {
+				var playerName = line.split(/(Player name:|Player Name:|player name:)/g)[2];
+				player = { 'Name': playerName }
+			}
+			if(line.includes('Tiers Played:') || line.includes('Tiers played:') || line.includes('tiers played:') || line.includes('Tiers:') || line.includes('tiers:')) {
+				if(line.toLowerCase().includes('sm ou') || line.toLowerCase().includes('sumo ou')){
 		    		player['SM OU'] = 'Y';
 		    	} else { 
 		    		player['SM OU'] = 'N';
 		    	}
-		    	if(tiers.toLowerCase().includes('sm ubers') || tiers.toLowerCase().includes('sumo ubers')){
+		    	if(line.toLowerCase().includes('sm ubers') || line.toLowerCase().includes('sumo ubers')){
 		    		player['SM Ubers'] = 'Y';
 		    	} else { 
 		    		player['SM Ubers'] = 'N';
 		    	}
-		    	if(tiers.toLowerCase().includes('sm lc') || tiers.toLowerCase().includes('sumo lc')){
+		    	if(line.toLowerCase().includes('sm lc') || line.toLowerCase().includes('sumo lc')){
 		    		player['SM LC'] = 'Y';
 		    	} else { 
 		    		player['SM LC'] = 'N';
 		    	}
-		    	if(tiers.toLowerCase().includes('doubles')){
+		    	if(line.toLowerCase().includes('doubles')){
 		    		player['Doubles'] = 'Y';
 		    	} else { 
 		    		player['Doubles'] = 'N';
 		    	}
-		    	if(tiers.toLowerCase().includes('oras')){
+		    	if(line.toLowerCase().includes('oras')){
 		    		player['ORAS OU'] = 'Y';
 		    	} else { 
 		    		player['ORAS OU'] = 'N';
 		    	}
-		    	if(tiers.toLowerCase().includes('bw')){
+		    	if(line.toLowerCase().includes('bw')){
 		    		player['BW OU'] = 'Y';
 		    	} else { 
 		    		player['BW OU'] = 'N';
 		    	}
-		    	if(tiers.toLowerCase().includes('dpp')){
+		    	if(line.toLowerCase().includes('dpp')){
 		    		player['DPP OU'] = 'Y';
 		    	} else { 
 		    		player['DPP OU'] = 'N';
 		    	}
-		    	if(tiers.toLowerCase().includes('adv')){
+		    	if(line.toLowerCase().includes('adv')){
 		    		player['ADV OU'] = 'Y';
 		    	} else { 
 		    		player['ADV OU'] = 'N';
 		    	}
-		    	if(tiers.toLowerCase().includes('gsc')){
+		    	if(line.toLowerCase().includes('gsc')){
 		    		player['GSC OU'] = 'Y';
 		    	} else { 
 		    		player['GSC OU'] = 'N';
 		    	}
-		    	if(tiers.toLowerCase().includes('rby')){
+		    	if(line.toLowerCase().includes('rby')){
 		    		player['RBY OU'] = 'Y';
 		    	} else { 
 		    		player['RBY OU'] = 'N';
 		    	}
-		    	$scope.players.push(player)
 			}
+			if(!line.includes('Last edited:')){
+				if(line.includes(', Today') || line.includes(', Yesterday') || line.includes(', Monday') || line.includes(', Tuesday') || line.includes(', Wednesday')
+						 || line.includes('Thursday') || line.includes(', Friday') || line.includes(', Saturday') || line.includes(', Sunday')
+						 || line.includes(', January') || line.includes(', Febuary') || line.includes(', March') || line.includes(', April') || line.includes(', May')
+						 || line.includes(', June') || line.includes(', July') || line.includes(', August') || line.includes(', September')
+						 || line.includes(', October') || line.includes(', November') || line.includes(', December')) {
+					if(player){
+						player['Name'] = line.split(/(, Today|, Yesterday|, Monday|, Tuesday|, Wednesday|, Thursday|, Friday|, Saturday|, Sunday|, January|, Febuary|, March|, April|, May|, June|, July|, August|, September|, October|, November|, December)/g)[0];
+						$scope.players.push(player);
+						player = null;
+					}
+				}
+			}
+				
 		});
-		
+			
 		const replacer = (key, value) => value === null ? '' : value;
 		const header = Object.keys($scope.players[0]);
 		let csv = $scope.players.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','));
