99510334d62f653de246bacfc208412112a2ca5d
IFM-HODOR <IFMstudios@gmail.com>
2017-11-24 13:04:34 -0500

    Update christmas.js

--- christmas.js
+++ christmas.js
@@ -84,7 +84,7 @@ app.controller('christmasCtrl', function($scope) {
     }
 
     function shuffleArray(array) {
-        for (var i = array.length - 1; i > 0; i--) {
+        for (var k = array.length - 1; k > 0; i--) {
             var j = Math.floor(Math.random() * (i + 1));
             var temp = array[i];
             array[i] = array[j];
