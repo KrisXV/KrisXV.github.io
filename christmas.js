a5d20dcdf9ce30dd033b7793781d8d81b5463439
IFM-HODOR <IFMstudios@gmail.com>
2017-11-24 12:45:51 -0500

    Create christmas.js

---
+++ christmas.js
@@ -0,0 +1,99 @@
+var app = angular.module('christmasApp', ['textAngular']);
+app.controller('christmasCtrl', function($scope) {
+	let irby = ['Dan', 'Kathy', 'Alex', 'Josh', 'Leah'];
+  let thomas = ['Scotty T', 'Susan', 'Evan', 'Collin'];
+  let vaughn = ['Billy', 'Linda'];
+  $scope.pairings = [];
+	
+	$scope.generatePairings = function(){
+      $scope.pairints = [];
+      
+	    var irby2 = shuffleArray(irby);
+      var thomas2 = shuffleArray(thomas);
+      var vaughn2 = shuffleArray(vaughn);
+      
+      let t = 'Thomas';
+      let v = 'Vaughn';
+      let i = 'Irby';
+      
+      var currentPerson, firstPerson, nextPerson, currentFamily, first;
+      
+      for(var j = 0; j<11, j++){
+        if(i == 0){
+           var startingFamily = getRandomInt(0, 3);
+           if(startingFamily == 0){
+              first = currentPerson = pop(irby2)
+              currentFamily = i;
+           } else if(startingFamily == 1){
+              first = currentPerson = pop(thomas2)
+              currentFamily = t;
+           } else {
+              first = currentPerson = pop(vaughn2);
+              currentFamily = v;
+           }
+        }
+        
+        var next = Math.round(Math.random);
+        if(currentFamily == t){
+           if(next == 0 && irby2.length){
+              nextPerson = pop(irby2);
+              currentFamily = i;
+           } else if(next == 1 && vaughn.length){
+              nextPerson = pop(vaughn2);
+              currentFamily = v;
+           } else if(thomas2.length){
+              $scope.generatePairings();
+           } else {
+              nextPerson = first;
+           }
+           pairings.push(currentPerson + ' > ' + nextPerson);
+        } else if(currentFamily == i){
+          if(next == 0 && thomas2.length){
+              nextPerson = pop(thomas2);
+              currentFamily = t;
+           } else if(next == 1 && vaughn.length){
+              nextPerson = pop(vaughn2);
+              currentFamily = v;
+           } else if(irby2.length){
+              $scope.generatePairings();
+           } else {
+              nextPerson = first;
+           }
+           pairings.push(currentPerson + ' > ' + nextPerson);
+        } else {
+          if(next == 0 && irby2.length){
+              nextPerson = pop(irby2);
+              currentFamily = i;
+           } else if(next == 1 & thomas.length){
+              nextPerson = pop(thomas2);
+              currentFamily = t;
+           } else if(vaughn2.length){
+              $scope.generatePairings();
+           } else {
+              nextPerson = first;
+           }
+           pairings.push(currentPerson + ' > ' + nextPerson);
+        }
+      }
+	}
+  
+  function pop(arr){
+     var last = arr[arr.length - 1);
+     arr = arr.splice(-1, 1);
+  }
+  
+  function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+  }
+  
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  }
+});
