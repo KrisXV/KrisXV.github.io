28e9f0afa5f4770daa0f2a13657210fd5d504a7e
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2024-03-20 13:39:18 -0600

    Fix things

--- js/smogonTourThreadGenerator.js
+++ js/smogonTourThreadGenerator.js
@@ -13,18 +13,14 @@ app.controller('threadCtrl', function($scope) {
 		$scope.initial = false;
 		var date = new Date();
 		var d = date.getDay();
-		if(d === 5) {
+		if (d === 5) {
 			$scope.tourNumber = 1;
 		}
-		if(d === 6) {
-			if (date.getHours() < 12) {
-				$scope.tourNumber = 2;
-			} else {
-				$scope.tourNumber = 3;
-			}
+		if (d === 6) {
+			$scope.tourNumber = 2;
 		}
-		if(d === 0) {
-			$scope.tourNumber = 4;
+		if (d === 0) {
+			$scope.tourNumber = 3;
 		}
 		$scope.title = "Season " + $scope.season + " (Week " + $scope.week + " [" + $scope.tier + " #" + $scope.tourNumber + "])";
 	}
